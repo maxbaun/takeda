d989c11cc0fe60691abfee9d664dd99a024427b1
Max Baun <max@d3applications.com>
2021-02-04 19:14:06 -0500

    common triggers copy update

--- src/components/pages/DiseaseBgPage/DiseaseBgPage.js
+++ src/components/pages/DiseaseBgPage/DiseaseBgPage.js
@@ -174,10 +174,10 @@ const DiseaseBgPage = props => {
       >
         <h2>Common triggers</h2>
         <p>
-          All patients with HAE should be educated about possible triggers that may cause a HAE attack.15 Although a
-          number of triggers have been identified, many HAE attacks occur without an obvious trigger, and the same
-          trigger may not always provoke a HAE attack.5 Therefore, HAE attacks may be unpredictable, and a personalized
-          management plan should be created in case of emergency.
+          All patients with HAE should be educated about possible triggers that may cause a HAE attack.<sup>16</sup>{' '}
+          Although a number of triggers have been identified, many HAE attacks occur without an obvious trigger, and the
+          same trigger may not always provoke a HAE attack.<sup>5</sup> Therefore, HAE attacks may be unpredictable, and
+          a personalized management plan should be created in case of emergency.
         </p>
       </CommonTriggers>
       <SymptomQuiz />
