6be45894c969e69f26927d2437625311fd1f2c28
Max Baun <max@d3applications.com>
2021-02-24 12:37:03 -0500

    download button mobile

--- src/components/Simulator/ActionBar/ActionBar.js
+++ src/components/Simulator/ActionBar/ActionBar.js
@@ -76,19 +76,24 @@ const BtnSecondary = styled.button`
   color: #000;
   display: flex;
   flex-flow: column nowrap;
+  font-size: 1.6rem;
+  font-weight: 700;
   grid-row: 2/2;
+  line-height: 1;
   padding: 0.5em 1.6em;
   position: relative;
   text-decoration: none;
   transition: none !important;
 
   ${mediaBreakpointUp('sm')} {
+    font-size: 2.3rem;
     padding: 1.9rem 3.6rem;
   }
 
   ${mediaBreakpointUp('lg')} {
     border: none;
     flex-flow: row nowrap;
+    font-size: 1.4rem;
     grid-row: initial;
     padding: 0;
   }
@@ -145,7 +150,6 @@ const BtnSecondary = styled.button`
   }
 
   .text {
-    font-size: 1.6rem;
     font-weight: 700;
     letter-spacing: 0.1em;
     line-height: 1.1;
@@ -171,10 +175,14 @@ const BtnSecondary = styled.button`
   }
 
   &.download {
-    display: none;
+    display: flex;
+    grid-column: 1/3;
+    grid-row: 3;
 
     ${mediaBreakpointUp('lg')} {
-      display: flex;
+      grid-column: initial;
+      grid-row: initial;
+      margin: 0;
     }
   }
 `;
@@ -190,7 +198,7 @@ const BtnGrid = styled.div`
   width: 100%;
 
   ${mediaBreakpointUp('sm')} {
-    grid-row-gap: 4.4.rem;
+    grid-row-gap: 4.4rem;
   }
 
   ${mediaBreakpointUp('lg')} {
