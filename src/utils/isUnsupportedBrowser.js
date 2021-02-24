72f1fec6274591b95a823af3cacaced9bf18e220
Max Baun <max@d3applications.com>
2021-02-24 10:37:59 -0500

    isunsupported browser issue

--- src/utils/isUnsupportedBrowser.js
+++ src/utils/isUnsupportedBrowser.js
@@ -1,7 +1,6 @@
 function isUnsupportedBrowser() {
-  return true;
   // IE 10 and IE 11
-  // return /Trident\/|MSIE/.test(window.navigator.userAgent);
+  return /Trident\/|MSIE/.test(window.navigator.userAgent);
 }
 
 export default isUnsupportedBrowser;
